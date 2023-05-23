1f125d7f4b8b3b3609b11a701836a0abb98a59c5
Artem Shamsiev <shamsievartyom@gmail.com>
2023-05-24 02:29:37 +0600

    fix: scroll in BurgerConstructorList

--- src/components/BurgerConstructorList/BurgerConstructorList.jsx
+++ src/components/BurgerConstructorList/BurgerConstructorList.jsx
@@ -10,17 +10,21 @@ function BurgerConstructorList({ data }) {
     };
 
     return (
-        <ul className={styles.list}>
-            {data.map((card, i) => {
-                if (i === 0) return (<ConstructorElement isLocked="true" type='top' extraClass='mr-4' key={card._id} text={card.name} thumbnail={card.image} price={card.price} />)
-                else if (i === data.length - 1) return (<ConstructorElement isLocked="true" type='bottom' extraClass='mr-4' key={card._id} text={card.name} thumbnail={card.image} price={card.price} />)
-                else return (
-                    <div className={styles.ingredient__container}>
-                        <DragIcon type="primary" />
-                        <ConstructorElement extraClass='mr-4' key={card._id} text={card.name} thumbnail={card.image} price={card.price} />
-                    </div>)
-            })}
-        </ul>
+        <div className={styles.main__container}>
+            <ConstructorElement extraClass={`mr-4 ${styles.ingredient__top}`} isLocked="true" type='top' key={data[0]?._id} text={data[0]?.name + " (верх)"} thumbnail={data[0]?.image} price={data[0]?.price} />
+            <ul className={styles.list}>
+                {data.map((card, i) => {
+                    if (i !== 0 && i !== data.length - 1) {
+                        return (
+                            <div className={styles.ingredient__container}>
+                                <DragIcon type="primary" />
+                                <ConstructorElement extraClass='mr-4' key={card._id} text={card.name} thumbnail={card.image} price={card.price} />
+                            </div>)
+                    }
+                })}
+            </ul>
+            <ConstructorElement extraClass={`mr-4 ${styles.ingredient__bottom}`} isLocked="true" type='bottom' key={data[0]?._id} text={data[0]?.name + " (низ)"} thumbnail={data[0]?.image} price={data[0]?.price} />
+        </div>
     )
 }
 
