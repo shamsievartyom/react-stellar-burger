ea6bf19e614a5b89f2f5f47a97a89dc981758c77
Artem Shamsiev <shamsievartyom@gmail.com>
2023-06-02 14:28:23 +0600

    fix: delete unnecessary dispatch

--- src/redux/thunks/App.js
+++ src/redux/thunks/App.js
@@ -2,14 +2,12 @@ import doFetch from "../../functions/doFetch";
 import { UPDATE_CARDS } from "../actions/BurgerIngredients";
 
 export const getIngredientsThunk = () => async (dispatch) => {
-    dispatch(
-        doFetch('ingredients', 'Get')
-            .then((res) => {
-                dispatch({
-                    type: UPDATE_CARDS,
-                    payload: res
-                });
-            })
-            .catch((err) => console.log(err))
-    );
+    doFetch('ingredients', 'Get')
+        .then((res) => {
+            dispatch({
+                type: UPDATE_CARDS,
+                payload: res
+            });
+        })
+        .catch((err) => console.log(err))
 }
