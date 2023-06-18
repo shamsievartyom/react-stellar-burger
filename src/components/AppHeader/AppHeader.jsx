5c7a954badcbecccbe94fe49443727641d3a6af2
Artem Shamsiev <shamsievartyom@gmail.com>
2023-06-18 19:09:04 +0600

    feature: active style for header buttons

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,30 +1,34 @@
+import { useRef } from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './AppHeader.module.css'
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 
 function AppHeader() {
+
+    const url = window.location.pathname
+
     return (
         <header className={styles.header}>
             <nav className={styles.navigation}>
                 <ul className={`mt-4 mb-4 ${styles.button__list}`}>
                     <li>
                         <NavLink to='/' className={`p-5 ${styles.button}`}>
-                            <BurgerIcon type="secondary" />
-                            <span className="text text_type_main-default text_color_inactive">Конструктор</span>
+                            <BurgerIcon type={url === '/' ? "primary" : "secondary"} />
+                            <span className={`text text_type_main-default text_color_inactive ${url === '/' ? styles.button_active : ''}`}>Конструктор</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to='something' className={`p-5 ${styles.button}`}>
-                            <ListIcon type="secondary" />
-                            <span className="text text_type_main-default text_color_inactive">Лента заказов</span>
+                            <ListIcon type={url === '/something' ? "primary" : "secondary"} />
+                            <span className={`text text_type_main-default text_color_inactive ${url === '/something' ? styles.button_active : ''}`}>Лента заказов</span>
                         </NavLink>
                     </li>
                 </ul>
             </nav>
             <Logo />
             <NavLink to="/profile" className={`mt-4 mb-4 p-5 ${styles.button} ${styles.button_type_profile}`}>
-                <ProfileIcon type="secondary" />
-                <span className="text text_type_main-default text_color_inactive">Личный кабинет</span>
+                <ProfileIcon type={url === '/profile' ? "primary" : "secondary"} />
+                <span className={`text text_type_main-default text_color_inactive ${url === '/profile' ? styles.button_active : ''}`}>Личный кабинет</span>
             </NavLink>
         </header>
     )
