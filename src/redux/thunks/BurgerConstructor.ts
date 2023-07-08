786378c09713f1184ac4eaf14f6334e29ba1e950
Artem Shamsiev <shamsievartyom@gmail.com>
2023-07-08 16:41:59 +0600

    feature: post oreder with accessToken

--- src/redux/thunks/BurgerConstructor.ts
+++ src/redux/thunks/BurgerConstructor.ts
@@ -2,9 +2,20 @@ import doFetch from "../../functions/doFetch";
 import { ADD_DATA_TO_ORDER_DETAILS, TOrderDetails } from "../actions/OrderDetails";
 import { TBurgerConstructor } from "../reducers/BurgerConstructor";
 import { AppDispatch, AppThunk } from "../types"
+import { fetchWithRefresh } from "./auth";
 
 export const sendOrderThunk: AppThunk = (constructorIngredients: TBurgerConstructor, openModal: Function) => async (dispatch: AppDispatch) => {
-    doFetch<TOrderDetails>('orders', 'POST', { ingredients: [constructorIngredients.bun?._id, ...constructorIngredients.ingredients.map((el) => el._id)] })
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("accessToken")
+        },
+        body: JSON.stringify({ ingredients: [constructorIngredients.bun?._id, ...constructorIngredients.ingredients.map((el) => el._id)] })
+    }
+
+    fetchWithRefresh('https://norma.nomoreparties.space/api/orders', options)
         .then((data) => {
             dispatch({
                 type: ADD_DATA_TO_ORDER_DETAILS,
